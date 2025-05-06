25346e98984b33308b167921b9b93506e16b0733
GustavoMedeiros <digital.gustavomedeiros@gmail.com>
2025-05-06 20:54:08 -0300

    Criado os métodos showErrorMessage(), verifyDecimalValue(), verifyDateTime() da classe Input

--- GestaoDeEquipamentosApp.ConsoleApp/Input.cs
+++ GestaoDeEquipamentosApp.ConsoleApp/Input.cs
@@ -8,5 +8,49 @@ namespace GestaoDeEquipamentosApp.ConsoleApp
 {
     internal class Input
     {
+        // mostra mensagem de erro padrão
+        public void showErrorMessage(string message)
+        {
+            Console.Clear();
+            Console.WriteLine($"\n Erro! {message}");
+            Console.WriteLine(" Aperte ENTER para continuar...");
+            Console.ReadLine();
+        }
+
+        // verifica se o input é um decimal
+        public decimal verifyDecimalValue(string message)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.Write(message);
+                string value = Console.ReadLine();
+
+                if (Decimal.TryParse(value, out decimal decimalValue))
+                {
+                    return decimalValue;
+                }
+                else
+                    showErrorMessage(" Esse não é um valor numérico.");
+            }
+        }
+
+        // verifica se o input é um DateTime
+        public DateTime verifyDateTime(string message)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.Write(message);
+                string value = Console.ReadLine();
+
+                if (DateTime.TryParse(value, out DateTime DateTimeValue))
+                {
+                    return DateTimeValue;
+                }
+                else
+                    showErrorMessage(" Esse não é um valor de data.");
+            }
+        }
     }
 }
