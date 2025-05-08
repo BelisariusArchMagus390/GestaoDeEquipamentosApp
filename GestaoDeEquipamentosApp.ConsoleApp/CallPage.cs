43c57ef40444ae19a183674b07be9abffea4f88e
GustavoMedeiros <digital.gustavomedeiros@gmail.com>
2025-05-08 17:09:35 -0300

    Criado o método showMenu() na classe CallPage

--- GestaoDeEquipamentosApp.ConsoleApp/CallPage.cs
+++ GestaoDeEquipamentosApp.ConsoleApp/CallPage.cs
@@ -10,10 +10,28 @@ namespace GestaoDeEquipamentosApp.ConsoleApp
     {
         public static CallDataBase Data;
         public static Input Input;
+        private static int IndexCount = 0;
 
         public CallPage(CallDataBase callData)
         {
             Data = callData;
+        }
+
+        public char showMenu()
+        {
+            Console.WriteLine(" --------------------------------------------");
+            Console.WriteLine($"\n GESTÃO DE CHAMADOS");
+            Console.WriteLine("\n --------------------------------------------");
+
+            Console.WriteLine("\n 1 - Registrar novo chamado");
+            Console.WriteLine(" 2 - Mostrar chamados");
+            Console.WriteLine(" 3 - Atualizar registro de chamado");
+            Console.WriteLine(" 4 - Excluir chamado");
+            Console.WriteLine(" 5 - Sair");
+
+            char option = Console.ReadLine()[0];
+
+            return option;
         }
     }
 }
