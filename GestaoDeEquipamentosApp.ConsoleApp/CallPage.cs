a7eb5c32b7840caa3fe1c9683151afe247db10ae
GustavoMedeiros <digital.gustavomedeiros@gmail.com>
2025-05-08 17:05:01 -0300

    Criado propriedades da classe CallPage

--- GestaoDeEquipamentosApp.ConsoleApp/CallPage.cs
+++ GestaoDeEquipamentosApp.ConsoleApp/CallPage.cs
@@ -8,5 +8,12 @@ namespace GestaoDeEquipamentosApp.ConsoleApp
 {
     internal class CallPage
     {
+        public static CallDataBase Data;
+        public static Input Input;
+
+        public CallPage(CallDataBase callData)
+        {
+            Data = callData;
+        }
     }
 }
