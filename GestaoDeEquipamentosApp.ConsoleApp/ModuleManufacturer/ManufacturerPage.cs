e68311fadedeab4978f89bd3b31bd3ffdb168448
GustavoMedeiros <digital.gustavomedeiros@gmail.com>
2025-05-26 21:04:54 -0300

    Refatorado a classe ManufacturerPage com a herança da classe PageModel

--- GestaoDeEquipamentosApp.ConsoleApp/ModuleManufacturer/ManufacturerPage.cs
+++ GestaoDeEquipamentosApp.ConsoleApp/ModuleManufacturer/ManufacturerPage.cs
@@ -1,4 +1,5 @@
 ﻿using GestaoDeEquipamentosApp.ConsoleApp.ModuleEquipment;
+using GestaoDeEquipamentosApp.ConsoleApp.ModuleShared;
 using GestaoDeEquipamentosApp.ConsoleApp.Utilities;
 using System;
 using System.Collections.Generic;
@@ -8,45 +9,44 @@ using System.Threading.Tasks;
 
 namespace GestaoDeEquipamentosApp.ConsoleApp.ModuleManufacturer
 {
-    internal class ManufacturerPage
+    public class ManufacturerPage : PageModel
     {
-        private static ManufacturerDataBase Data;
-        public static Input Input = new Input();
-        private static int IndexCount = 1;
-
-        public void setData(ManufacturerDataBase data)
+        private ManufacturerDataBase Data;
+    
+        public ManufacturerPage(ManufacturerDataBase Data)
+            : base("Fabricante", Data)
         {
-            Data = data;
+            this.Data = Data;
         }
 
-        public ManufacturerDataBase getData()
+        public override void showRegisters(bool showForSelection = false)
         {
-            return Data;
+            Console.WriteLine(
+                " {0, -10} | {1, -20} | {2, -10} | {3, -10} ",
+                " Id", "Nome", "E-mail", "Telefone"
+            );
+
+            foreach (Manufacturer m in Data.selectRegister())
+            {
+                if (m == null)
+                    continue;
+
+                Console.WriteLine(
+                    " {0, -10} | {1, -20} | {2, -10} | {3, -10} ",
+                    " "+m.Id, m.Name, m.Email, m.Telephone
+                );
+            }
+
+            if (showForSelection == false)
+            {
+                Console.WriteLine("\n Aperte ENTER para continuar...");
+                Console.ReadLine();
+            }
         }
 
-        public char showMenu()
-        {
-            Console.WriteLine(" --------------------------------------------");
-            Console.WriteLine($"\n GESTÃO DE FABRICANTES");
-            Console.WriteLine("\n --------------------------------------------");
-
-            Console.WriteLine("\n 1 - Registrar novo fabricante");
-            Console.WriteLine(" 2 - Mostrar fabricantes");
-            Console.WriteLine(" 3 - Atualizar registro de fabricante");
-            Console.WriteLine(" 4 - Excluir registro de fabricante");
-            Console.WriteLine(" 5 - Sair");
-
-            Console.Write("\n Escolha uma das opções acima: ");
-            char option = Console.ReadLine()[0];
-
-            return option;
-        }
-
-        public void register()
+        protected override EntityModel getDate()
         {
             Manufacturer manufacturer = new Manufacturer();
-
-            manufacturer.Id = IndexCount;
 
             Console.Clear();
             Console.Write("\n Digite o nome do fabricante: ");
@@ -60,115 +60,7 @@ namespace GestaoDeEquipamentosApp.ConsoleApp.ModuleManufacturer
             Console.Write("\n Digite o telefone do fabricante: ");
             manufacturer.Telephone = Console.ReadLine();
 
-            Data.Manufacturers.Add(manufacturer);
-            IndexCount++;
-
-            Console.Clear();
-            Console.WriteLine("\n Fabricante registrado com sucesso!");
-            Console.WriteLine("\n Aperte ENTER para continuar...");
-            Console.ReadLine();
-        }
-
-        public int findIndexManufacturer()
-        {
-            int index = 0;
-
-            while (true)
-            {
-                Console.Clear();
-
-                showManufacturers();
-
-                Console.Write("\n Entre com o ID do fabricante: ");
-                int id = int.Parse(Console.ReadLine());
-
-                bool manufacturerFound = false;
-                foreach (Manufacturer m in Data.Manufacturers)
-                {
-                    if (m.Id == id)
-                    {
-                        Data.Manufacturers.IndexOf(m);
-                        manufacturerFound = true;
-                        break;
-                    }
-                }
-
-                if (manufacturerFound == true)
-                    break;
-                else
-                    Input.showErrorMessage("Esse fabricante não existe.");
-            }
-            return index;
-        }
-
-        public void edit()
-        {
-            int manufacturerIndex = findIndexManufacturer();
-
-            Console.Clear();
-            Console.Write("\n Digite o nome do fabricante: ");
-            Data.Manufacturers[manufacturerIndex].Name = Console.ReadLine();
-
-            Console.Clear();
-            Console.Write("\n Digite o e-mail do fabricante: ");
-            Data.Manufacturers[manufacturerIndex].Email = Console.ReadLine();
-
-            Console.Clear();
-            Console.Write("\n Digite o telefone do fabricante: ");
-            Data.Manufacturers[manufacturerIndex].Telephone = Console.ReadLine();
-
-            Console.Clear();
-            Console.WriteLine("\n Registrado de fabricante atualizado com sucesso!");
-            Console.WriteLine("\n Aperte ENTER para continuar...");
-            Console.ReadLine();
-        }
-
-        public void delete()
-        {
-            int manufacturerIndex = findIndexManufacturer();
-
-            Data.Manufacturers.RemoveAt(manufacturerIndex);
-
-            Console.Clear();
-            Console.WriteLine("\n Registro de fabricante removido com sucesso!");
-            Console.WriteLine("\n Aperte ENTER para continuar...");
-            Console.ReadLine();
-        }
-
-        public void showManufacturers(bool showForSelection = false)
-        {
-            EquipmentDataBase equipmentData = new EquipmentDataBase();
-            List<Equipment> equipmentsList = equipmentData.Equipments;
-
-            Console.WriteLine(
-                " {0, -10} | {1, -20} | {2, -10} | {3, -10} | {4, -20}",
-                " Id", "Nome", "E-mail", "Telefone", "Quantidade de equipamentos"
-            );
-
-            foreach (Manufacturer m in Data.Manufacturers)
-            {
-                if (m == null)
-                    continue;
-
-                int contEquipment = 0;
-
-                for (int i = 0; i < equipmentsList.Count; i++)
-                {
-                    if (m.Name == equipmentsList[i].Manufacturer.Name)
-                        contEquipment++;
-                }
-
-                Console.WriteLine(
-                    " {0, -10} | {1, -20} | {2, -10} | {3, -10} | {4, -20}",
-                    " "+m.Id, m.Name, m.Email, m.Telephone, contEquipment
-                );
-            }
-
-            if (showForSelection == false)
-            {
-                Console.WriteLine("\n Aperte ENTER para continuar...");
-                Console.ReadLine();
-            }
+            return manufacturer;
         }
     }
 }
